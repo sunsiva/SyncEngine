638432de0ea2d6fc5b39afead548960a40be9995
sunsiva <sivaprakasams@gmail.com>
2020-09-14 18:27:14 +0530

    Job batch file added

---
+++ Job/gitautocommit.bat
@@ -0,0 +1,5 @@
+c:
+cd C:\Users\sundarsi\me\xtra\Cache Technologies\Gokare LPO\POC\gitapp\techno-links\
+"%PROGRAMFILES%\git\bin\sh.exe" --login -i -c "git commit -am 'AutoCommitMessage'"
+git push origin master
+exit
