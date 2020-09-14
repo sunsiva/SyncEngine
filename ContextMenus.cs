5412b5a24179137f5f9583d2d722bbaa947ae29f
sunsiva <sivaprakasams@gmail.com>
2020-09-14 18:20:46 +0530

    first commit

---
+++ ContextMenus.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SyncTrayApp.Properties;
+using System.Drawing;
+
+namespace SyncTrayApp
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	class ContextMenus
+	{
+		/// <summary>
+		/// Is the About box displayed?
+		/// </summary>
+		bool isAboutLoaded = false;
+		bool isSettingsLoaded = false;
+
+		/// <summary>
+		/// Creates this instance.
+		/// </summary>
+		/// <returns>ContextMenuStrip</returns>
+		public ContextMenuStrip Create()
+		{
+			// Add the default menu options.
+			ContextMenuStrip menu = new ContextMenuStrip();
+			ToolStripMenuItem item;
+			ToolStripSeparator sep;
+
+			// About.
+			item = new ToolStripMenuItem();
+			item.Text = "About";
+			item.Click += new EventHandler(About_Click);
+			item.Image = Resources.About;
+			menu.Items.Add(item);
+
+			// Windows Explorer.
+			item = new ToolStripMenuItem();
+			item.Text = "Settings";
+			item.Click += new EventHandler(Explorer_Click);
+			item.Image = Resources.Explorer;
+			menu.Items.Add(item);
+
+			// Sync.
+			item = new ToolStripMenuItem();
+			item.Text = "Sync";
+			item.Click += new EventHandler(Sync_Click);
+			item.Image = Resources.sync;
+			menu.Items.Add(item);
+
+			// Separator.
+			sep = new ToolStripSeparator();
+			menu.Items.Add(sep);
+
+			// Exit.
+			item = new ToolStripMenuItem();
+			item.Text = "Exit";
+			item.Click += new System.EventHandler(Exit_Click);
+			item.Image = Resources.Exit;
+			menu.Items.Add(item);
+
+			return menu;
+		}
+
+		/// <summary>
+		/// Handles the Click event of the Explorer control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void Explorer_Click(object sender, EventArgs e)
+		{
+			if (!isSettingsLoaded)
+			{
+				isSettingsLoaded = true;
+				new Settings().ShowDialog();
+				isSettingsLoaded = false;
+			}
+			//Process.Start("explorer", null);
+		}
+
+		/// <summary>
+		/// Handles the Click event of the About control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void About_Click(object sender, EventArgs e)
+		{
+			if (!isAboutLoaded)
+			{
+				isAboutLoaded = true;
+				new AboutBox().ShowDialog();
+				isAboutLoaded = false;
+			}
+		}
+
+		/// <summary>
+		/// Handles the Click event of the About control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void Sync_Click(object sender, EventArgs e)
+		{
+			Process.Start(@"C:\Users\sundarsi\me\xtra\Cache Technologies\Gokare LPO\POC\gitautocommit.bat", null);
+		}
+
+		/// <summary>
+		/// Processes a menu item.
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void Exit_Click(object sender, EventArgs e)
+		{
+			// Quit without further ado.
+			Application.Exit();
+		}
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SyncTrayApp
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                saveFile(textBox1.Text);
+                _ = folderDlg.RootFolder;
+
+                
+            }
+        }
+
+        private void saveFile(string msg)
+        {
+            //once you have the path you get the directory with:
+            var directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Save File to .txt  
+            FileStream fParameter = new FileStream(directory+"\\log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+            m_WriterParameter.Write(msg);
+            m_WriterParameter.Flush();
+            m_WriterParameter.Close();
+        }
+
+    }
+}
